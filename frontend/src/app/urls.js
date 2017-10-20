875e5a79ce9c2bf8e6df79b189313b6d3098f078
Valerii Kovalchuk <kovvalole@gmail.com>
2017-10-21 02:02:15 +0300

    Create dummy forum page

--- frontend/src/app/urls.js
+++ frontend/src/app/urls.js
@@ -4,16 +4,18 @@ import {IndexRedirect, Route} from "react-router";
 import Admin from "app/layouts/Admin";
 import RouteNotFound from "app/components/RouteNotFound";
 import users from "app/users/urls";
+import Forum from "app/forum/components/Forum";
 
 
 const urls = (
     <Route path="/">
-        <IndexRedirect to="admin/users"/>
+        <IndexRedirect to="forum"/>
         <Route component={Admin} path="admin">
             <IndexRedirect to="users"/>
             {users}
             <Route path="*" component={RouteNotFound}/>
         </Route>
+        <Route path="/forum" component={Forum}/>
     </Route>
 );
 
