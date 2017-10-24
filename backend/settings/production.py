3d4a3e2c86bc58364ff85704c56b528cdb9d493d
Valerii Kovalchuk <kovvalole@gmail.com>
2017-10-24 23:11:56 +0300

    Turn off debug on prod, fix import

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -5,7 +5,7 @@ import dj_database_url
 SECRET_KEY = dotenv.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
