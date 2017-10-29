efa8d3e699b1b95a983f7dc367661dd9ac994570
Valerii Kovalchuk <kovvalole@gmail.com>
2017-10-29 12:20:52 +0200

    Ability to change own password #39

--- backend/urls.py
+++ backend/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth.views import login, logout_then_login
 from django.contrib import admin
 
 from .views import app, index
+from users.change_password_view import change_password
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -10,5 +11,6 @@ urlpatterns = [
     url(r'^app/', app, name='app'),
     url('^auth/login/$', login, name='login'),
     url('^auth/logout/$', logout_then_login, name='logout'),
+    url('^auth/change_password/', change_password, name='change_password'),
     url('^$', index, name='index'),
 ]
