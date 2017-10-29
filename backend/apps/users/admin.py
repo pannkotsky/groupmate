0b52b52cccb715b6d38d52191c7fb6cbaefa3337
Valerii Kovalchuk <kovvalole@gmail.com>
2017-10-29 09:45:19 +0200

    Add phone fields to user model #3

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -5,6 +5,6 @@ from .models import EmailUser
 
 @admin.register(EmailUser)
 class EmailUserAdmin(admin.ModelAdmin):
-    fields = ('email', 'first_name', 'last_name', 'is_staff', 'is_active',)
+    fields = ('email', 'phone', 'first_name', 'last_name', 'is_staff', 'is_active',)
     readonly_fields = ('date_joined', 'last_updated',)
-    list_display = ('email', 'get_full_name')
+    list_display = ('email', 'phone', 'get_full_name')
--- backend/settings/common.py
+++ backend/settings/common.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 LOCAL_APPS = [
+    "common",
     "users"
 ]
 
